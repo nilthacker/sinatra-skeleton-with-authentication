ce046a28edaa4b0fbbe45f414f0c8e519c90beec
nil <nilthacker@users.noreply.github.com>
2016-01-11 14:23:37 -0800

    Move user creation routes into users controller to be more RESTful

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,3 +1,23 @@
+get '/users/new' do
+  if session[:user_id]
+    @user = User.find(session[:user_id])
+    redirect "/users/#{@user.id}"
+  else
+    erb :register
+  end
+end
+
+post '/users/new' do
+  @user = User.new(first_name: params[:first_name], last_name: params[:last_name], email: params[:email])
+  @user.password = params[:password_plaintext]
+  if @user.save
+    redirect "/sessions/new"
+  else
+    @errors = @user.errors.full_messages
+    erb :register
+  end
+end
+
 get '/users/:user_id' do
   @logged_in_as = User.find(session[:user_id]) if session[:user_id]
   @viewing_user = User.find(params[:user_id])
